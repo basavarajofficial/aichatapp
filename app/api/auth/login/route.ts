5e66accbf4c0b6ce1bf192b09399179980220f1a
BasavarajM <bmannangi34@gmail.com>
2025-02-06 10:42:44 +0530

    fixed bugs with JWT

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -14,8 +14,8 @@ export async function POST(req: Request) {
     const user = users.find((u) => u.email === email);
 
     if (!user || !user.password){
-            return NextResponse.json({ message: "User not found!" }, { status: 404 });
-        }
+      return NextResponse.json({ message: "User not found!" }, { status: 404 });
+    }
 
     const passwordCheck = await bcrypt.compare(password, user.password);
 
@@ -23,19 +23,20 @@ export async function POST(req: Request) {
       return NextResponse.json({ message: "Invalid credentials" }, { status: 401 });
     }
 
-    // Generate JWT token
-    const token = generateToken({ id: user.id, email: user.email });
+    //  Await the generated token
+    const token = await generateToken({ id: user.id, email: user.email });
 
-    // Set the token as a cookie
-    (await cookies()).set("token", token, {
-        httpOnly: true,
-        // secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 3600, // 1 hour
-        path: "/",
-      });
+    //  Set the token as a cookie
+    cookies().set("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 3600, // 1 hour
+      path: "/",
+    });
 
-      return NextResponse.json({ message: "Login successful!!!" }, { status: 200 });
+    return NextResponse.json({ message: "Login successful!!!" }, { status: 200 });
+
   } catch (error) {
     return NextResponse.json({ message: "Internal Server Error", error }, { status: 500 });
   }
